eea9583d63a9a43581f93b3382eca67d0b42e740
Keari Eggers <kungfu.keari@gmail.com>
2015-07-28 16:11:00 -0500

    completed first four functions in arrays.js

--- app/arrays.js
+++ app/arrays.js
@@ -3,18 +3,38 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+    
+    return arr.indexOf(item);
+    
   },
 
   sum : function(arr) {
 
+    var sum = 0;
+
+    arr.forEach(function(elem) {
+      sum += elem;
+    });
+
+    return sum;
+    
   },
 
   remove : function(arr, item) {
+    
+    return arr.filter(function(value) {
+      return value !== item;
+    });
 
   },
 
   removeWithoutCopy : function(arr, item) {
+    
+    while(arr.indexOf(item) !== -1) {
+      arr.splice(arr.indexOf(item), 1);
+    }
+
+    return arr;
 
   },
 
