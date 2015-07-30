175bb0342c8a46beb48ad86e10a7aa561bc88e80
Keari Eggers <kungfu.keari@gmail.com>
2015-07-30 15:05:25 -0500

    finished arrays section

--- app/arrays.js
+++ app/arrays.js
@@ -39,42 +39,125 @@ exports.arraysAnswers = {
   },
 
   append : function(arr, item) {
+    
+    var array = [];
+    
+    arr.forEach(function(elem) {
+      array.push(elem);
+    });
+    
+    array.push(item);
+    
+    return array;
 
   },
 
   truncate : function(arr) {
+    
+    return arr.filter(function(elem, index) {
+      return index !== (arr.length - 1);
+    })
 
   },
 
   prepend : function(arr, item) {
+    
+    var array = [item];
+    
+    arr.forEach(function(elem) {
+      array.push(elem);
+    });
+
+    return array;
 
   },
 
   curtail : function(arr) {
+    
+    return arr.filter(function(elem, index) {
+      return index !== 0;
+    })
 
   },
 
   concat : function(arr1, arr2) {
+    
+    return arr1.concat(arr2);
 
   },
 
   insert : function(arr, item, index) {
+    
+    var array = [];
+    
+    arr.forEach(function(elem, i) {
+      
+      if(i === index) {
+        array.push(item);
+      }
+      
+      array.push(elem);
+      
+    });
+    
+    return array;
 
   },
 
   count : function(arr, item) {
+    
+    var len = arr.length,
+        cnt = 0;
+    
+    for(var i = 0; i < len; i += 1) {
+      
+      if(arr[i] === item) {
+        cnt += 1;
+      }
+        
+    }
+    
+    return cnt;
 
   },
 
   duplicates : function(arr) {
+    
+    var array = [];
+    
+    arr.forEach(function(elem, index) {
+      
+      if(index !== arr.lastIndexOf(elem) && array.indexOf(elem) === -1) {
+        array.push(elem);
+      }
+      
+    });
+
+    return array;
 
   },
 
   square : function(arr) {
+    
+    return arr.map(function(value) {
+      return value * value;
+    });
 
   },
 
   findAllOccurrences : function(arr, target) {
+    
+    var occurences = [];
+    
+    arr.forEach(function(elem, index) {
+      
+      if(elem === target) {
+        occurences.push(index);
+      }
+      
+    });
+    
+    return occurences;
 
   }
 };
